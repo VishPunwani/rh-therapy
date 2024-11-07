c8f4c539af676de227d1643d4bde73e68815ba52
Vishnu Punwani <vishnupunwani20@gmail.com>
2024-11-07 00:11:02 -0800

    navbar

--- rh-counselling/src/components/Contact.jsx
+++ rh-counselling/src/components/Contact.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Contact.css';
+import Navbar from './NavBar';
 
 const ContactPage = () => {
   return (
     <div>
-      <header className="header">
-        <div className="logo">RH Counselling</div>
-        <nav className="nav">
-          <Link className="navLink" to="/">Home</Link>
-          <Link className="navLink" to="/about">About</Link>
-          <Link className="navLink" to="/services">Services & Rates</Link>
-          <Link className="navLink" to="/contact">Contact</Link>
-          <Link className="navLink" to="/book-online">Book Online</Link>
-        </nav>
-      </header>
 
       <div className="container">
         <aside className="sidebar">
--- rh-counselling/src/components/NavBar.jsx
+++ rh-counselling/src/components/NavBar.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Contact.css';
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="brand-container">
-                <Link className="brand-logo" to="/">
-                    {/* <img src={Logo} alt="Logo" /> */}
-                    YourBlogCoach
-                </Link>
-            </div>
-            <div className="navbar-container">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/service">Services</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </div>
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">RH Counselling</div>
+        <nav className="nav">
+          <Link className="navLink" to="/">Home</Link>
+          <Link className="navLink" to="/about">About</Link>
+          <Link className="navLink" to="/services">Services & Rates</Link>
+          <Link className="navLink" to="/contact">Contact</Link>
+          <Link className="navLink" to="/book-online">Book Online</Link>
         </nav>
-    );
+      </header>
+      </div>
+  );
 }
+
 
 export default Navbar;
