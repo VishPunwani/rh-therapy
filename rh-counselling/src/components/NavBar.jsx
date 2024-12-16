ee83cc50ced54e89cab47732aba686a21e31fac6
Vishnu Punwani <vishnupunwani20@gmail.com>
2024-12-16 12:47:37 -0800

    logo and booking link addition

--- rh-counselling/src/components/NavBar.jsx
+++ rh-counselling/src/components/NavBar.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Contact.css';
+import logo from '../img/navbar_logo.png';
 
 const Navbar = () => {
   return (
     <div>
       <header className="header">
-        <div className="logo">RH Counselling</div>
+        <div className="logo">
+          {/* Use the imported 'logo' variable */}
+          <img src={logo} alt="Logo" />
+        </div>
         <nav className="nav">
           <Link className="navLink" to="/">Home</Link>
           <Link className="navLink" to="/about">About</Link>
           <Link className="navLink" to="/services">Services & Rates</Link>
           <Link className="navLink" to="/contact">Contact</Link>
-          <Link className="navLink" to="/book-online">Book Online</Link>
+          <Link className="navLink" to="https://neuralive.janeapp.com/locations/langford-bc-office/book#/staff_member/20">Book Online</Link>
         </nav>
       </header>
-      </div>
+    </div>
   );
-}
-
+};
 
 export default Navbar;
